96a26915aafb4be39ca92cc69d2854fb605a4113
Sahin Deniz <seahindeniz@gmail.com>
2021-06-19 13:04:20 +0300

    first commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  collectCoverage: true,
+  roots: ['./src'],
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  // https://regex101.com/r/tiSozd/1
+  testRegex: '\\.(test|spec)\\.tsx?$',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
